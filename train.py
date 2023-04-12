1d8becf8b477eb78696e0ec6676bc754614a915a
LTTdouble <130349905+LTTdouble@users.noreply.github.com>
2023-04-12 09:19:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ def main():
     val_loader = DataLoader(dataset=val_set, num_workers=opt.threads, batch_size=1, shuffle=False)
  
     model = SRDNet(opt)
-    weights_init(model)
+    #weights_init(model)
 
     criterion = nn.L1Loss()
     HFL_loss=HFL()
