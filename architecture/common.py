8a3c70f3a6012b194d8ea7cab7f11bcc3db6d1b1
LTTdouble <130349905+LTTdouble@users.noreply.github.com>
2024-03-01 09:39:27 +0800

    Update common.py

--- architecture/common.py
+++ architecture/common.py
@@ -9,26 +9,6 @@ def default_conv(in_channels, out_channels, kernel_size, bias=True):
         in_channels, out_channels, kernel_size,
         padding=(kernel_size//2), bias=bias))
 
-class ResBlock(nn.Module):
-    def __init__(self, conv, n_feats, kernel_size, bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
-        super(ResBlock, self).__init__()
-        m = []
-        for i in range(2):
-            m.append(conv(n_feats, n_feats, kernel_size, bias=bias))
-            if bn:
-                m.append(nn.BatchNorm2d(n_feats))
-            if i == 0:
-                m.append(act)
-
-        self.body = nn.Sequential(*m)
-        self.res_scale = res_scale
-
-    def forward(self, x):
-        res = self.body(x).mul(self.res_scale)
-        res += x
-
-        return res
-
 
 class ResBlock(nn.Module):
     def __init__(
